e02e673075c43c797bb0c138b51c45965847afc3
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2021-10-13 23:21:37 +0300

    firsr commit

---
+++ src/main/java/com/javademo/javaspringazuredemo/JavaSpringAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javademo.javaspringazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAzureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringAzureDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javademo/javaspringazuredemo/JavaSpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javademo.javaspringazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
